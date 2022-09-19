5808c36064e63c220e78d3247f7a156c28880f08
Edward Ross <edward@skeptric.com>
2022-09-19 22:12:29 +1000

    Use TopologicalSorter to find root nodes

--- bookfinder/transform.py
+++ bookfinder/transform.py
@@ -1,3 +1,4 @@
+from graphlib import TopologicalSorter
 from typing import TypeVar
 
 T = TypeVar("T")
@@ -6,13 +7,11 @@ T = TypeVar("T")
 def get_root_mapping(parent_dict: dict[T, T]) -> dict[T, T]:
     """Return a mapping of each node to its root node."""
     root_dict = {}
-
-    for item, parent in parent_dict.items():
-        while parent in parent_dict:
-            grandparent = parent_dict[parent]
-            if parent == grandparent:
-                break
-            parent = grandparent
-        root_dict[item] = parent
-
+    for node in TopologicalSorter(
+        {k: [v] for k, v in parent_dict.items()}
+    ).static_order():
+        if node not in parent_dict:
+            root_dict[node] = node
+        else:
+            root_dict[node] = root_dict[parent_dict[node]]
     return root_dict
--- test/test_transform.py
+++ test/test_transform.py
@@ -4,4 +4,4 @@ from bookfinder.transform import get_root_mapping
 def test_get_root_mapping():
     parent_dict = {1: 2, 2: 3, 4: 5}
     root_dict = get_root_mapping(parent_dict)
-    assert root_dict == {1: 3, 2: 3, 4: 5}
+    assert root_dict == {1: 3, 2: 3, 3: 3, 4: 5, 5: 5}
